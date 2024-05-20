c4bc547703a06c8e35d1cf94b409a38c3baf4aea
2gaudy <agrant9946@gmail.com>
2024-05-20 13:09:40 -0400

    its aliveee

--- VideoHosting/src/App.tsx
+++ VideoHosting/src/App.tsx
@@ -12,7 +12,9 @@ function App() {
         <div className='video-wrapper'>
           <h2>The Video</h2>
 
-            <VideoPlayer/>
+            <VideoPlayer width={1280} height={720}/>
+            <iframe src="https://drive.google.com/file/d/1_dNZkGDGQoaPHsJHzzm3EhAoweSwLYsg/preview" width="640" height="480" allow="autoplay"></iframe>
+
 
         </div>
 
--- VideoHosting/src/components/VideoPlayer.jsx
+++ VideoHosting/src/components/VideoPlayer.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useRef } from 'react';
 
-function VideoPlayer() {
+function VideoPlayer(props) {
+    const { width, height } = props;
     const cloudinaryRef = useRef();
     const videoRef = useRef();
     useEffect(() => {
         if (cloudinaryRef.current ) return;
         cloudinaryRef.current = window.cloudinary
-        cloudinaryRef.current.VideoPlayer(videoRef.current,  {
+        cloudinaryRef.current.videoPlayer(videoRef.current,  {
             cloud_name: 'dhgnili4u'
         })
     }, [])
@@ -14,8 +15,11 @@ function VideoPlayer() {
         <video
             ref={videoRef}
             data-cld-public-id="qiljpohwvu6dfgflg5du"
+            width={width}
+            height={height}
+            controls
         />
     )
 }
 
-// export VideoPlayer;
+export default VideoPlayer;
